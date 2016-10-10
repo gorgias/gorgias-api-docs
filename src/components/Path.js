3d7148bf31f2b7ce6afa76878f04b11c28a69e09
Martin Latrille <martin@gorgias.io>
2016-10-10 16:53:32 +0200

    updated openapi.json, sorted Tags and Definitions in leftbar; greatly improved display of example responses

--- src/components/Path.js
+++ src/components/Path.js
@@ -20,8 +20,6 @@ const Verb = ({verb, method, uri}) => (
                 <h3 className="text-right">HTTP Request</h3>
                 <code className="code">{method.toUpperCase()} {uri}</code>
 
-                <h3 className="text-right">Example Response (status: {status}) </h3>
-                <code className="code"></code>
                 <Responses responses={verb.get('responses')}  />
             </div>
         </div>
@@ -29,13 +27,160 @@ const Verb = ({verb, method, uri}) => (
 )
 
 export const Responses = ({responses}) => {
-    
-    return (<div></div>)
+    return (
+        <div>
+            {
+                responses.entrySeq().map((entry, idx) => (
+                    <Response
+                        key={idx}
+                        status={entry[0]}
+                        responseRef={entry[1]}
+                    />
+                )).toJS()
+            }
+        </div>
+    )
 }
 
-export const Response = ({responseRef}) => {
-    console.log(responses)
-    return (<div></div>)
+/**
+ * Take an OpenAPI definition and turn it in an example with fake values.
+ *
+ * @param schema: the definition to transform
+ * @returns {*} the example with fake values
+ */
+const examplify = (schema) => {
+    if (typeof(schema) === 'string') {
+        return schema
+    }
+
+    if (schema.get('type') && !schema.getIn(['type', 'type'])) {  // type.type is schemas with a real `type` field (like `actions` for ex.)
+        if (schema.get('default')) {
+            return schema.get('default')
+        } else if (schema.getIn(['meta', 'enum'])) {
+            return schema.getIn(['meta', 'enum', 0])
+        } else {
+            switch (schema.get('type')) {
+                case 'string': {
+                    if (schema.get('format') === 'date-time') {
+                        return '2016-10-07T07:38:36'
+                    } else if (schema.get('format') === 'url') {
+                        return 'https://gorgias.io/'
+                    } else {
+                        return 'string'
+                    }
+                }
+
+                case 'boolean': {
+                    return false
+                }
+
+                case 'integer': {
+                    return 1
+                }
+
+                case 'array': {
+                    if (schema.getIn(['items', '$ref'])) {
+                        if (schema.getIn(['meta', 'only'])) {
+                            return examplify(
+                                getDefinitionProperties(
+                                    schema.getIn(['items', '$ref']),
+                                    schema.getIn(['meta', 'only'])
+                                )
+                            )
+                        }
+
+                        return [{id: 1}]
+                    } else if (schema.getIn(['items', 'type'])) {
+                        return [examplify(schema.getIn(['items', 'type']))]
+                    } else {
+                        console.log('SOMETHING WENT WRONG', schema.toJS())
+                        break
+                    }
+                }
+
+                default:
+                    return schema
+            }
+        }
+    } else if (schema.get('$ref')) {
+        if (schema.getIn(['meta', 'only'])) {
+            return examplify(
+                getDefinitionProperties(
+                    schema.get('$ref'),
+                    schema.getIn(['meta', 'only'])
+                )
+            )
+        }
+
+        return {id: 1} //schema.get('$ref').split('/')[2] //examplify(getDefinition(schema.get('$ref')).get('properties'), nestLevel + 1)
+    }
+
+    return schema.map(value => examplify(value))
+}
+
+/**
+ * Take a ref to a definition and return its properties.
+ *
+ * @param ref: the ref to the definition
+ * @param only (optional): the list of fields to return
+ * @returns {*}: the definition, total or partial
+ */
+const getDefinitionProperties = (ref, only = null) => {
+    if (!ref) {
+        return ref
+    }
+
+    let response = openapi
+    let path = ref.split('/')
+    path.shift()  // remove the first `#`
+
+    for (var key of path) {
+        response = response.get(key)
+    }
+
+    response = response.get('properties')
+
+    console.log(only)
+
+    if (only) {
+        response = response.filter((value, key) => only.includes(key))
+    }
+
+    return response
+}
+
+export const Response = ({status, responseRef}) => {
+    let response = responseRef
+
+    if (typeof(responseRef) !== 'string' && responseRef.get('schema')) {
+        const schema = responseRef.get('schema')
+        let ref = null
+        let transformInArray = false
+
+        if (schema.get('$ref')) {
+            ref = schema.get('$ref')
+        } else if (schema.get('type') === 'array') {
+            ref = schema.getIn(['items', '$ref'])
+            transformInArray = true
+        }
+
+        response = examplify(getDefinitionProperties(ref))
+
+        if (transformInArray) {
+            response = [response]
+        }
+
+        response = JSON.stringify(response, null, 4)
+    }
+
+    return (
+        <div>
+            <h3 className="text-right">{status}</h3>
+            <pre className="code">
+                {response}
+            </pre>
+        </div>
+    )
 }
 
 export const Parameters = ({parameters}) => {
@@ -45,7 +190,7 @@ export const Parameters = ({parameters}) => {
 
     return (
         <table className="ui very basic collapsing celled table">
-            
+
             <thead>
             <tr>
                 <th>Name</th>
@@ -58,6 +203,7 @@ export const Parameters = ({parameters}) => {
                 <Parameter key={i} paramRef={paramRef}/>
             )).toList()}
             </tbody>
+
         </table>
     )
 }
