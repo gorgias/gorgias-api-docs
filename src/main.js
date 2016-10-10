73042dc8385840c2eeb15545cd2c586170bace28
Martin Latrille <martin@gorgias.io>
2016-10-10 16:53:32 +0200

    Overall Upgrade

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Router, browserHistory} from 'react-router'
+import {fromJS} from 'immutable'
+import axios from 'axios'
 import {routes} from './routes'
 
-ReactDOM.render(
-    <Router history={browserHistory} children={routes} />,
-    document.getElementById('root')
-)
+const PROD_URL = 'https://gorgias.gorgias.io/doc/openapi.json'
+const DEV_URL = 'http://acme.gorgias.docker/doc/openapi.json'
+
+axios.get(DEV_URL)
+    .then((json = {}) => json.data)
+    .then(resp => {
+        window.openapi = fromJS(resp)
+
+        ReactDOM.render(
+            <Router history={browserHistory}
+                    children={routes}
+            />,
+            document.getElementById('root')
+        )
+    })
